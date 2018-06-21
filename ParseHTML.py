142a65c47deeba631c70eee7283b19d77035caa1
Tor Hadas <torhadas@mail.huji.ac.il>
2018-06-21 19:22:31 +0300

    ParseHTML to soup

--- ParseHTML.py
+++ ParseHTML.py
@@ -1,14 +1,16 @@
-properties
-topic
-tables = document.getElementsByClassName("classMainTableBox")
-for table in tables:
-	for link in table.getElementsByTagName("a"):
-		if(link.innerHTML == properties["topic"]):
-			# go to link.href
+def getTopicURL(page, properties):
+	tables = page(class_="classMainTableBox")
+	for table in tables:
+		for link in table("a"):
+			if(link.get_text() == properties["topic"]):
+				return link.get('href')
 
-table = document.getElementsByClassName("classMainTable")[0]
-years = table.getElementsByClassName("classMainTitle")
-for year in years:
-	if year.children[0].innerHTML.find(properties["year"]) != -1:
-		
+def getArticlesURL(page, properties)
+	years = page(class_="classMainTable")[0](class_="classMainTitle")
+	for year in years:
+		if years[0].get_text().find("2017") != -1:
+			for month in year.parent("td"):
+				if(month.get_text().find(properties["month"]) != -1):
+					return month("a")[0].get('href')
+
 

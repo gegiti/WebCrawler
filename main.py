f6988f4adfbc1729aa9a6e809b666142a35f6656
Tomer Goren <tomergoren99@gmail.com>
2018-06-21 19:23:26 +0300

    main + test

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-if __name__ == '__main__':
-    addr = "https://www.ynet.co.il/home/0,7340,L-8,00.html"
-    page = requests.get(addr)
+
+def get_properties():
+    properties = dict()
+    properties["topic"] = raw_input("What topic? ")
+    properties["year"] = raw_input("What year? ")
+    properties["month"] = raw_input("What month? ")
+    return properties
+
+def get_soup(url):
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    print(soup.title)
+    return soup
+
+
+if __name__ == '__main__':
+    addr = "https://www.ynet.co.il/home/0,7340,L-4269,00.html"
+    soup = get_soup(addr)
+    properties = get_properties()
+
